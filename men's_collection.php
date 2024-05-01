0b14f60b59fbb6adcea5809361a203bed18c9284
Sandaru Fernando <sandarufernando2002@gmail.com>
2024-05-01 17:30:58 +0530

    second commit

--- men's_collection.php
+++ men's_collection.php
@@ -1,7 +1,35 @@
 <?php
     require_once 'connection.php';
 
-    $sql = "SELECT * FROM product";
+    $sortOrder = "price ASC";
+
+    if(isset($_GET['sort']) && $_GET['sort'] == 'low_high') {
+        $sortOrder = "ORDER BY price ASC";
+    } elseif(isset($_GET['sort']) && $_GET['sort'] == 'high_low') {
+        $sortOrder = "ORDER BY price DESC";
+    } elseif(isset($_GET['sort']) && $_GET['sort'] == 'nike') {
+        $sortOrder = "AND brand='Nike'";
+    } elseif(isset($_GET['sort']) && $_GET['sort'] == 'lacoste') {
+        $sortOrder = "AND brand='Lacoste'";
+    } elseif(isset($_GET['sort']) && $_GET['sort'] == 'adidas') {
+        $sortOrder = "AND brand='Adidas'";
+    } elseif(isset($_GET['sort']) && $_GET['sort'] == 'newBalance') {
+        $sortOrder = "AND brand='New Balance'";
+    } elseif(isset($_GET['sort']) && $_GET['sort'] == 'airJordan') {
+        $sortOrder = "AND brand='Air Jordan'";
+    } elseif(isset($_GET['sort']) && $_GET['sort'] == 'white') {
+        $sortOrder = "AND color1='white' OR color2='white'";
+    } elseif(isset($_GET['sort']) && $_GET['sort'] == 'black') {
+        $sortOrder = "AND color1='black' OR color2='black'";
+    } elseif(isset($_GET['sort']) && $_GET['sort'] == 'green') {
+        $sortOrder = "AND color1='green' OR color2='green'";
+    } elseif(isset($_GET['sort']) && $_GET['sort'] == 'yellow') {
+        $sortOrder = "AND color1='yellow' OR color2='yellow'";
+    } elseif(isset($_GET['sort']) && $_GET['sort'] == 'blue') {
+        $sortOrder = "AND color1='blue' OR color2='blue'";
+    }
+
+    $sql = "SELECT * FROM product WHERE product_id BETWEEN 1 AND 10 $sortOrder";
     $all_product = $conn->query($sql);
 ?>
 
@@ -18,18 +46,21 @@
     <title>Men's Shoes & Sneakers</title>
 </head>
 <body>
-    <nav class="navbar">
-        <img id="logo" src="imgs/logo.png">
-        <ul>
-            <li><a href="">Home</a></li>
-            <li><a href="">About Us</a></li>
-            <li><a href="">Products</a></li>
-            <li><a href="">FAQ</a></li>
-            <li><a href="">Contact Us</a></li>
-        </ul>
-        <a href=""><img id="user" src="imgs/user.png"></a>
-        <a href=""><img id="cart" src="imgs/cart.png"></a>
-    </nav>
+    <header class="header1">
+        <div >
+            <img class="image2 logo" src="imgs/diagonal_white_gradient_bkg.png" alt="alt text" />
+            <img class="image3 logo" src="imgs/footprint_brand_logo_blue.png" alt="alt text" />
+        </div>
+        <div class="topnav">
+            <a href="./homepage.php">Home</a>
+            <a href="#aboutus">About Us</a>
+            <a href="#ourcollection">Products</a>
+            <a href="">FAQ</a>
+            <a href="#footer">Contact Us</a>
+            <a href="./create.php"><img id="user" src="imgs/user.png" alt="alt text"/></a>
+            <a href="./cart.php"><img id="cart" src="imgs/cart.png" alt="alt text"/></a>
+        </div>
+    </header>
 
     <div class="header">
         <h1>Men's Shoes & Sneakers</h1>
@@ -41,28 +72,73 @@
     </div>
 
     <div class="sort">
-        <p>Size &nbsp;&nbsp;&nbsp; color &nbsp;&nbsp;&nbsp;brand&nbsp;&nbsp;&nbsp; sortby</p>
+        <div style="width: 70%; display:flex; gap: 20px">
+            <div class="dropdown">
+                <button>Size: Any</button>
+                <div class="content">
+                    <a href="">6</a>
+                    <a href="">7</a>
+                    <a href="">8</a>
+                    <a href="">9</a>
+                    <a href="">10</a>
+                </div>
+            </div>
+
+            <div class="dropdown">
+                <button>Color: Any</button>
+                <div class="content">
+                    <a href="?sort=white">White</a>
+                    <a href="?sort=black">Black</a>
+                    <a href="?sort=blue">Blue</a>
+                    <a href="?sort=green">Green</a>
+                    <a href="?sort=yellow">Yellow</a>
+                </div>
+            </div>
+
+            <div class="dropdown">
+                <button>Brand: Any</button>
+                <div class="content">
+                    <a href="?sort=nike">Nike</a>
+                    <a href="?sort=adidas">Adidas</a>
+                    <a href="?sort=lacoste">Lacoste</a>
+                    <a href="?sort=airJordan">Air Jordan</a>
+                    <a href="?sort=newBalance">New Balance</a>
+                </div>
+            </div>
+        </div>
+
+        <div style="width: 30%; margin-left: 600px">
+            <div class="dropdown">
+                <button style="width: 250px;">Sort by: Any</button>
+                <div class="content">
+                    <a href="?sort=high_low" style="width: 226px;">Price: High-Low</a>
+                    <a href="?sort=low_high" style="width: 226px;">Price: Low-High</a>
+                </div>
+            </div>
+        </div>
     </div>
 
     <main>
         <?php
-            while($row = mysqli_fetch_assoc($all_product)){
+            for ($i=0; $i<2; $i++){
+                while($row = mysqli_fetch_assoc($all_product)){
         ?>
-        <div class="card">
-            <a href="">
+        <div class="card" data-product-id="<?php echo $row["product_id"]; ?>">
+            
                 <div class="image">
                     <img src="<?php echo $row["image1"]; ?>" alt="">
                 </div>
-            </a>
-            <a href="">
+            
                 <div class="caption">
                     <p><?php echo $row["brand"]; ?></p>
-                    <p><?php echo $row["product_name"]; ?></p>
-                    <p><?php echo $row["price"]; ?></p>
+                    <p style="color: rgba(58, 59, 64, 1);"><?php echo $row["product_name"]; ?></p>
+                    <p>Rs. <?php echo $row["price"]; ?>.00</p>
                 </div>
-            </a>
+            
         </div>
         <?php
+                }
+                mysqli_data_seek($all_product, 0);
             }
         ?>
     </main>
@@ -72,5 +148,19 @@
             
         </div>
     </footer>
+
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            var productElements = document.querySelectorAll('.card');
+
+            productElements.forEach(function(productElement) {
+                productElement.addEventListener('click', function() {
+                    var productId = this.dataset.productId;
+
+                    window.location.href = 'product_detail.php?product_id=' + productId;
+                })
+            })
+        })
+    </script>
 </body>
 </html>
---
+++ product_detail.php
@@ -0,0 +1,140 @@
+<?php
+    require_once 'connection.php';
+
+    if (isset($_GET['product_id'])){
+        $product_id = $_GET['product_id'];
+
+        $sql = "SELECT * FROM product WHERE product_id = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("i", $product_id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows > 0){
+            $product = $result->fetch_assoc();
+        }else{
+            echo "Product not found.";
+            exit();
+        }
+    }else{
+        echo "Product ID not provided.";
+        exit();
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style1.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <title>Men's Shoes & Sneakers</title>
+</head>
+<body>
+<header class="header1">
+        <div >
+            <img class="image2 logo" src="imgs/diagonal_white_gradient_bkg.png" alt="alt text" />
+            <img class="image3 logo" src="imgs/footprint_brand_logo_blue.png" alt="alt text" />
+        </div>
+        <div class="topnav">
+            <a href="./homepage.php">Home</a>
+            <a href="#aboutus">About Us</a>
+            <a href="#ourcollection">Products</a>
+            <a href="">FAQ</a>
+            <a href="#footer">Contact Us</a>
+            <a href="./create.php"><img id="user" src="imgs/user.png" alt="alt text"/></a>
+            <a href="./cart.php"><img id="cart" src="imgs/cart.png" alt="alt text"/></a>
+        </div>
+    </header>
+
+
+    <main style="justify-content: space-evenly; margin-bottom: 150px; margin-top: 30px">
+        <div class="product_images">
+            <div>
+                <div class="small">
+                    <img id="image1" src="<?php echo $product['image1']; ?>" onclick="changeImage1()">
+                </div>
+                <div class="small">
+                    <img id="image2" src="<?php echo $product['image2']; ?>" onclick="changeImage2()">
+                </div>
+                <div class="small">
+                    <img id="image3" src="<?php echo $product['image3']; ?>" onclick="changeImage3()">
+                </div>
+                <div class="small">
+                    <img id="image4" src="<?php echo $product['image4']; ?>" onclick="changeImage4()">
+                </div>
+            </div>
+            <div class="big">
+                <img id="image5" src="<?php echo $product['image1']; ?>">
+            </div>
+        </div>
+        <div class="verticle_line"></div>
+        <div class="product_details">
+            <h3><?php echo $product['brand']; ?></h3>
+            <h1><?php echo $product['product_name']; ?></h1>
+            <p>Men's Shoes</p>
+            <p style="font-size: 18px; margin-bottom: 50px"><b>Rs. <?php echo $product['price']; ?>.00</b></p>
+            <p>Size</p>
+            <div class="sizes">
+                <div>6</div>
+                <div>7</div>
+                <div>8</div>
+                <div>9</div>
+                <div>10</div>
+            </div>
+            <div class="counter_btn">
+                <div class="counter">
+                    <div class="op">-</div>
+                    <div>1</div>
+                    <div class="op">+</div>
+                </div>
+                <div>
+                    <button>ADD TO CART</button>
+                </div>
+            </div>
+            
+            <p style="cursor: pointer;" id="wishlist"><img src="imgs/heart.png"> Add to Wishlist</p>
+
+            <p>Shown: <?php echo $product['color1']; ?>/<?php echo $product['color2']; ?></p>
+            <p>Style: <?php echo $product['model']; ?></p>
+        </div>
+    </main>
+
+    <footer>
+        <div class="footer">
+            
+        </div>
+    </footer>
+
+    <script>
+        function changeImage2(){
+            let displayImage = document.getElementById('image5')
+            if (!displayImage.src.match('<?php echo $product['image2']; ?>')){
+                displayImage.src = '<?php echo $product['image2']; ?>'
+            }
+        }
+        function changeImage1(){
+            let displayImage = document.getElementById('image5')
+            if (!displayImage.src.match('<?php echo $product['image1']; ?>')){
+                displayImage.src = '<?php echo $product['image1']; ?>'
+            }
+        }
+        function changeImage3(){
+            let displayImage = document.getElementById('image5')
+            if (!displayImage.src.match('<?php echo $product['image3']; ?>')){
+                displayImage.src = '<?php echo $product['image3']; ?>'
+            }
+        }
+        function changeImage4(){
+            let displayImage = document.getElementById('image5')
+            if (!displayImage.src.match('<?php echo $product['image4']; ?>')){
+                displayImage.src = '<?php echo $product['image4']; ?>'
+            }
+        }
+    </script>
+</body>
+</html>
